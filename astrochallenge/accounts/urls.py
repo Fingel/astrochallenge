99439654e2766398b0a93819029dc3aeb5956e44
Austin Riba <root@austinriba.com>
2015-02-07 13:36:29 -0800

    login logout

--- astrochallenge/accounts/urls.py
+++ astrochallenge/accounts/urls.py
@@ -4,4 +4,7 @@ from astrochallenge.accounts import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="logout"),
+
 )
