862ae2e4dd83821bf0f2d6bb8510a3e5269ef7a5
Austin Riba <root@austinriba.com>
2015-02-08 15:44:28 -0800

    signal to create profile on user creation

--- astrochallenge/accounts/models.py
+++ astrochallenge/accounts/models.py
@@ -1,5 +1,7 @@
 import pytz
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from timezone_field import TimeZoneField
 
@@ -7,7 +9,15 @@ from timezone_field import TimeZoneField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, editable=False)
     timezone = TimeZoneField(default="UTC")
-    location = models.CharField(max_length=200, blank=True)
-    lat = models.DecimalField("latitude", max_digits=10, decimal_places=7, blank=True)
-    lng = models.DecimalField("longitude", max_digits=10, decimal_places=7, blank=True)
-    profile_text = models.TextField(blank=True)
+    location = models.CharField(max_length=200, blank=True, default="")
+    lat = models.DecimalField("latitude", max_digits=10, decimal_places=7, blank=True, null=True)
+    lng = models.DecimalField("longitude", max_digits=10, decimal_places=7, blank=True, null=True)
+    profile_text = models.TextField(blank=True, default="")
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, **kwargs):
+    try:
+        instance.userprofile
+    except:
+        UserProfile(user=instance).save()
