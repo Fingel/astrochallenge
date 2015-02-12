1d7e102bedbdf352ed24e5b2a973ea0dbf953991
Austin Riba <root@austinriba.com>
2015-02-12 14:28:51 -0800

    astroobject detail

--- astrochallenge/objects/urls.py
+++ astrochallenge/objects/urls.py
@@ -4,11 +4,13 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 
-from astrochallenge.objects.models import Constellation
+from astrochallenge.objects.models import Constellation, AstroObject
+from astrochallenge.objects.views import DSODetailView
 
 urlpatterns = patterns('',
     url(r'^objects/$', TemplateView.as_view(template_name="objects/index.html"), name="object-index"),
     url(r'^objects/constellations/$', ListView.as_view(model=Constellation), name="constellation-list"),
     url(r'^objects/constellations/(?P<slug>\w+)/$', DetailView.as_view(model=Constellation, slug_field="abbreviation"), name='constellation-detail'),
-
+    url(r'^objects/dso/(?P<pk>\d+)/$', DSODetailView.as_view(), name="dso-detail"),
+    url(r'^objects/dso/(?P<catalog>\w+)/(?P<designation>\d+)/$', DSODetailView.as_view(), name="dso-detail"),
 )
--- astrochallenge/objects/views.py
+++ astrochallenge/objects/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from models import Constellation, CatalogObject, AstroObject
 
+
+class DSODetailView(DetailView):
+    def get_object(self):
+        if self.kwargs.get('pk'):
+            return get_object_or_404(AstroObject, pk=self.kwargs['pk'])
+        catalog_object = get_object_or_404(CatalogObject, catalog=self.kwargs['catalog'], designation=self.kwargs['designation'])
+        return catalog_object.astro_object
