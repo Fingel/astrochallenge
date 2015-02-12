959c028cd91399023fde8a64867fedf3bee38ecc
Austin Riba <root@austinriba.com>
2015-02-12 15:20:50 -0800

    astroobject detail

---
+++ astrochallenge/objects/management/commands/importimages.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.core.files import File
+from astrochallenge.objects.models import AstroObject, CatalogObject, Constellation
+from decimal import *
+import urllib
+import csv
+
+
+class Command(BaseCommand):
+    args = '<catalog> <action>'
+    help = "Import object images"
+
+    def handle(self, *args, **options):
+        catalog = args[0]
+        for catalog_object in CatalogObject.objects.filter(catalog=catalog):
+            astro_object = catalog_object.astro_object
+            if args[1] == "download":
+                if catalog == "M":
+                    image = urllib.urlretrieve('http://messier.seds.org/Jpg/m{0}.jpg'.format(i))
+                    astro_object.image.save('m{0}.jpg'.format(i), File(open(image[0])), save=False)
+                    astro_object.save()
+                    print "downloaded and set image for M{0}".format(catalog_object.designation)
+                else:
+                    pass
+            else:
+                if catalog == "M":
+                    astro_object.image = 'astro_objects/m{0}.jpg'.format(catalog_object.designation)
+                    astro_object.save()
+                    print "set existing image for M{0}".format(catalog_object.designation)
--- astrochallenge/objects/urls.py
+++ astrochallenge/objects/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     url(r'^objects/$', TemplateView.as_view(template_name="objects/index.html"), name="object-index"),
     url(r'^objects/constellations/$', ListView.as_view(model=Constellation), name="constellation-list"),
     url(r'^objects/constellations/(?P<slug>\w+)/$', DetailView.as_view(model=Constellation, slug_field="abbreviation"), name='constellation-detail'),
-    url(r'^objects/dso/(?P<pk>\d+)/$', DSODetailView.as_view(), name="dso-detail"),
-    url(r'^objects/dso/(?P<catalog>\w+)/(?P<designation>\d+)/$', DSODetailView.as_view(), name="dso-detail"),
+    url(r'^objects/dso/(?P<pk>\d+)/$', DSODetailView.as_view(), name="astroobject-detail"),
+    url(r'^objects/dso/(?P<catalog>\w+)/(?P<designation>\d+)/$', DSODetailView.as_view(), name="astroobject-detail"),
 )
