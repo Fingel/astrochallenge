3cdfbe7c0ec7dc78d907b8366057a84d92a53023
Austin Riba <root@austinriba.com>
2015-03-05 12:47:54 -0800

    fix moon phase

--- astrochallenge/objects/utils.py
+++ astrochallenge/objects/utils.py
@@ -16,7 +16,7 @@ import ephem
 def moon_phase(when):
     tau = 2 * ephem.pi
     waxing = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0"]
-    waning = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+    waning = ["0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
     waning.reverse()
     sun = ephem.Sun()
     moon = ephem.Moon()
