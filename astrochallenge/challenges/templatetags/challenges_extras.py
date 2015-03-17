d5d4cccf732713c79ef75fd5432f74d4921aff36
Austin Riba <root@austinriba.com>
2015-03-17 11:29:10 -0700

    making things look nice

--- astrochallenge/challenges/templatetags/challenges_extras.py
+++ astrochallenge/challenges/templatetags/challenges_extras.py
@@ -8,3 +8,13 @@ register = template.Library()
 @register.filter
 def has_completed_challenge(user, challenge):
     return True if CompletedChallenge.objects.filter(user_profile=user.userprofile, challenge=challenge).exists() else False
+
+
+@register.filter
+def rating_stars(rating):
+    html = ""
+    for i in range(0, rating):
+        html += '<span class="glyphicon glyphicon-star"></span>'
+    for i in range(0, 5 - rating):
+        html += '<span class="glyphicon glyphicon-star-empty"></span>'
+    return html
