4e5583428c9802fe7bfb4a53b07f9dd246b8a45d
Austin Riba <root@austinriba.com>
2015-03-19 16:42:58 -0700

    contact form and current time

--- astrochallenge/accounts/urls.py
+++ astrochallenge/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView
 
 
 from astrochallenge.accounts import views
@@ -8,6 +9,7 @@ from astrochallenge.objects.models import Observation
 urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
     url(r'^$', views.index, name='index'),
+    url(r'^contact/$', TemplateView.as_view(template_name="accounts/contact.html"), name="contact"),
     url(r'^accounts/edit/$', views.edit_profile, name='edit-profile'),
     url(r'^accounts/profile/(?P<username>\w+)/$', views.profile, name='profile'),
     url(r'^observations/(?P<slug>\d+)/$', DetailView.as_view(model=Observation, slug_field='pk'), name='observation-detail'),
