e705aab380801e973ad4a4fa6515fa9ebd65869e
Austin Riba <root@austinriba.com>
2015-04-04 05:33:41 -0700

    make username url match any character

--- astrochallenge/accounts/urls.py
+++ astrochallenge/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.ContactView.as_view(), name="contact"),
     url(r'^accounts/edit/$', views.edit_profile, name='edit-profile'),
-    url(r'^accounts/profile/(?P<username>\w+)/$', views.profile, name='profile'),
+    url(r'^accounts/profile/(?P<username>.+)/$', views.profile, name='profile'),
     url(r'^equipment/add/$', views.add_equipment, name='add-equipment'),
     url(r'^equipment/delete/(?P<pk>\d+)/$', views.delete_equipment, name='delete-equipment'),
 )
