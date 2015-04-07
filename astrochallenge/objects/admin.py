18a6cd4ffb86fff757153bc6f39c5a7d87e66408
Austin Riba <root@austinriba.com>
2015-04-06 18:01:11 -0700

    make admin interface a little more friendly

--- astrochallenge/objects/admin.py
+++ astrochallenge/objects/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from astrochallenge.objects.models import AstroObject, CatalogObject, Constellation, SolarSystemObject, Observation
 
-admin.site.register(AstroObject)
+
+class AstroObjectAdmin(admin.ModelAdmin):
+    list_display = ('common_name', 'catalog_rep')
+    list_filter = ('type', 'constellation')
+    search_fields = ('common_name', 'description')
+
+
+class SolarSystemObjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('type',)
+    search_fields = ('name', 'description')
+
+admin.site.register(AstroObject, AstroObjectAdmin)
 admin.site.register(CatalogObject)
 admin.site.register(Constellation)
 admin.site.register(Observation)
-admin.site.register(SolarSystemObject)
+admin.site.register(SolarSystemObject, SolarSystemObjectAdmin)
