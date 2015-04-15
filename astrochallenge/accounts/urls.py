7d189f46a55fce69dc9918e5b7ef8173f35058ef
Austin Riba <root@austinriba.com>
2015-04-15 14:05:19 -0700

    add user directory

--- astrochallenge/accounts/urls.py
+++ astrochallenge/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic.list import ListView
 
 from astrochallenge.accounts import views
+from django.contrib.auth.models import User
 
 urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
@@ -11,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^equipment/add/$', views.add_equipment, name='add-equipment'),
     url(r'^equipment/delete/(?P<pk>\d+)/$', views.delete_equipment, name='delete-equipment'),
     url(r'^accounts/kudos/(?P<observation>\d+)/$', views.give_kudos, name='give-kudos'),
+    url(r'^accounts/users/$', ListView.as_view(model=User, template_name="accounts/user_list.html"), name='user-list'),
 )
