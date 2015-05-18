9948fca650e628c1d5d0cc1ce3dbec68e5d2d6fd
Austin Riba <root@austinriba.com>
2015-05-18 09:34:15 -0700

    factory boy and more tests

---
+++ astrochallenge/accounts/test_helpers.py
@@ -0,0 +1,26 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class AdminFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = 'admin'
+    first_name = 'Admin'
+    last_name = 'User'
+    password = factory.PostGenerationMethodCall('set_password', 'supersecret')
+
+    is_superuser = True
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    username = 'stargazer'
+    password = factory.PostGenerationMethodCall('set_password', 'supersecret')
+    first_name = 'Edwin'
+    last_name = 'Hubble'
+    is_active = True
+    email = factory.Sequence(lambda n: 'gazer{0}@example.com'.format(n))
--- astrochallenge/accounts/test_views.py
+++ astrochallenge/accounts/test_views.py
@@ -2,12 +2,52 @@ from django.core.urlresolvers import reverse
 from django.test import TransactionTestCase
 from django.contrib.auth.models import User
 from registration.models import RegistrationProfile
+from test_helpers import AdminFactory, UserFactory
 
 
 class AccountsViewTest(TransactionTestCase):
     def setUp(self):
-        # Create an admin user
-        User.objects.create(username='admin', password='password')
+        AdminFactory.build().save()
+        UserFactory.build().save()
+
+    def test_homepage(self):
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertIn("Latest Observations", response.content)
+
+        # Logged in
+        login_result = self.client.login(username='stargazer', password='supersecret')
+        self.assertTrue(login_result)
+        response = self.client.get(reverse('index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertIn("Latest Observations", response.content)
+
+    def test_login(self):
+        response = self.client.get(reverse('auth_login'))
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('Login', response.content)
+
+        data = {'username': 'notauser', 'password': 'supersecret'}
+
+        response = self.client.post(reverse('auth_login'), data, follow=True)
+        self.assertEquals(response.status_code, 200)
+        self.assertIn("Your username and password didn't match. Please try again.",
+                      response.content)
+
+        data['username'] = 'stargazer'
+        response = self.client.post(reverse('auth_login'), data, follow=True)
+        self.assertEquals(response.status_code, 200)
+        # Make sure the user is logged in
+        self.assertIn('_auth_user_id', self.client.session)
+
+    def test_logout(self):
+        login_result = self.client.login(username='stargazer', password='supersecret')
+        self.assertTrue(login_result)
+        response = self.client.get(reverse('auth_logout'))
+        self.assertEquals(response.status_code, 200)
+        self.assertIn("You have been sucessfully logged out", response.content)
+        # Make sure the user is logged out
+        self.assertIsNone(self.client.session.get('_auth_user_id'))
 
     def test_signup(self):
         response = self.client.get(reverse('registration_register'))
@@ -39,6 +79,9 @@ class AccountsViewTest(TransactionTestCase):
                                    follow=True)
         self.assertEquals(response.status_code, 200)
         self.assertIn("Yesss! Your account is activated.", response.content)
+
+        # Make sure the user is logged in
+        self.assertIsNotNone(self.client.session.get('_auth_user_id'))
 
         response = self.client.get('index')
         self.assertIn("Warning! You have not set your latitude/longitude!", response.content)
