065d05a9b478a8663b086511ecc1a703eeb58f9c
Austin Riba <root@austinriba.com>
2015-06-18 08:25:23 -0700

    remove jobs for now

--- astrochallenge/settings.py
+++ astrochallenge/settings.py
@@ -4,7 +4,6 @@ Django settings for astrochallenge project.
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from datetime import timedelta
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
@@ -225,10 +224,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_ENABLE_UTC = True
 
 CELERYBEAT_SCHEDULE = {
-    'add-every-30-seconds': {
-        'task': 'debug_task',
-        'schedule': timedelta(seconds=10),
-    },
+
 }
 
 CELERY_TIMEZONE = 'UTC'
