dec0ba66161a566dece34702df66986c9d488047
Austin Riba <root@austinriba.com>
2015-06-25 11:02:04 -0700

    supernova admin

--- astrochallenge/objects/admin.py
+++ astrochallenge/objects/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from astrochallenge.objects.models import AstroObject, CatalogObject, Constellation, SolarSystemObject, Observation
+from astrochallenge.objects.models import (AstroObject, CatalogObject,
+    Constellation, SolarSystemObject, Observation, Supernova, SupernovaMagnitude)
 
 
 class CatalogObjectInline(admin.StackedInline):
     model = CatalogObject
+
+
+class SupernovaMagnitudeInline(admin.StackedInline):
+    model = SupernovaMagnitude
+
+
+class SupernovaAdmin(admin.ModelAdmin):
+    inlines = (SupernovaMagnitudeInline,)
+    list_display = ('name', 'sntype')
+    list_filter = ('sntype',)
+    search_fields = ('name',)
 
 
 class AstroObjectAdmin(admin.ModelAdmin):
@@ -23,3 +35,4 @@ admin.site.register(CatalogObject)
 admin.site.register(Constellation)
 admin.site.register(Observation)
 admin.site.register(SolarSystemObject, SolarSystemObjectAdmin)
+admin.site.register(Supernova, SupernovaAdmin)
