ece05e5a47e1ef7e272734921f8d1b2dabc78056
Austin Riba <root@austinriba.com>
2015-07-07 18:35:20 -0700

    add .png extension

--- astrochallenge/challenges/urls.py
+++ astrochallenge/challenges/urls.py
@@ -6,6 +6,6 @@ from astrochallenge.challenges.views import ChallengeListView, SigImageView
 
 urlpatterns = patterns('',
     url(r'^challenges/$', ChallengeListView.as_view(model=Challenge), name="challenge-list"),
-    url(r'^challenges/sigimage/$', SigImageView.as_view(), name="sigimage"),
+    url(r'^challenges/sigimage\.png$', SigImageView.as_view(), name="sigimage"),
     url(r'^challenges/(?P<slug>\d+)/$', DetailView.as_view(model=Challenge, slug_field="pk"), name="challenge-detail"),
 )
